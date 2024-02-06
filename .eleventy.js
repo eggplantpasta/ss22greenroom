4a25d79b1a9a6bdb55995676c396b027ddb8589a
Brett Roper <roper.brett@gmail.com>
2024-02-06 08:58:32 +0800

    Initial 11ty conversion

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function (eleventyConfig) {
+    return {
+        dir: {
+            input: 'src',
+            output: 'docs', // set as docs for github pages publishing
+        },
+    };
+};
